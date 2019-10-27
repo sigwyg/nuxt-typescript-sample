0ad6b7702d6f3232cd2599c34c04109d34663814
sigwyg <sigwyg@users.noreply.github.com>
2019-10-27 21:28:19 +0900

    global css

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  "semi": false,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "printWidth": 120,
-  tabWidth: 2
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  overrides: [
+    {
+      files: ['*.html', '*.scss', '*.css'],
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,29 @@
 export default {
   srcDir: 'src/',
+  rootDir: './',
   typescript: {
     typeCheck: {
       eslint: true,
     },
   },
+  head: {
+    title: 'nuxt-ts-form sample',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+    ],
+  },
+  /**
+   * Global CSS
+   */
+  css: ['normalize.css/normalize.css', { src: '~/assets/styles/main.scss', lang: 'scss' }],
+  /**
+   * build
+   */
   buildModules: ['@nuxt/typescript-build'],
   build: {
+    extractCSS: true,
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
